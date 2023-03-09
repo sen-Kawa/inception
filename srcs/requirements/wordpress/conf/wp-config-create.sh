573e218908aaa808302e4961f697794743caeb65
debian <universalexperimental@protonmail.com>
2023-03-09 15:33:47 -0500

    wordpress config

---
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -0,0 +1,23 @@
+#!bin/sh
+if [ ! -f "/var/www/wp-config.php" ]; then
+cat << EOF > /var/www/wp-config.php
+<?php
+define( 'MYSQL_NAME', '${MYSQL_NAME}' );
+define( 'MYSQL_USER', '${MYSQL_USER}' );
+define( 'MYSQL_PASSWORD', '${MYSQL_PASSWORD}' );
+define( 'DB_HOST', 'mariadb' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define('FS_METHOD','direct');
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+define( 'ABSPATH', __DIR__ . '/' );}
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
+define( 'WP_REDIS_DATABASE', 0 );
+require_once ABSPATH . 'wp-settings.php';
+EOF
+fi
