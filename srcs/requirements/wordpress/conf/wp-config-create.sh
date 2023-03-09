1fa34ad4769744aaff15ddefccb1d5cc69cf9a7f
debian <universalexperimental@protonmail.com>
2023-03-09 17:58:06 -0500

    trying to stablish failed connection with envs

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -2,15 +2,18 @@
 if [ ! -f "/var/www/wp-config.php" ]; then
 cat << EOF > /var/www/wp-config.php
 <?php
-define( 'MYSQL_NAME', '${MYSQL_NAME}' );
-define( 'MYSQL_USER', '${MYSQL_USER}' );
-define( 'MYSQL_PASSWORD', '${MYSQL_PASSWORD}' );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+@ini_set( 'display_errors', 1 );
+define( 'DB_NAME', '${MYSQL_NAME}' );
+define( 'DB_USER', '${MYSQL_USER}' );
+define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
 define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
 \$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 define( 'WP_REDIS_HOST', 'redis' );
