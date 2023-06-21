97882800fae3073bf75293820118a3917c6c6eb5
debian <universalexperimental@protonmail.com>
2023-06-21 13:39:40 +0200

    trying to run a shell script from cli installation of wordpress

---
+++ srcs/requirements/wordpress/conf/install.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+wp core install --url=kaheinz.42.fr --title=Inception --admin_user=kaheinz --admin_password=docker --admin_email=ka@42.fr --skip-email
+
+wp user create pedro pedro@42.fr --user_pass=docker
