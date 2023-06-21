67af2d4be32ee65ccaa8b756403c81a8d8648ff3
debian <universalexperimental@protonmail.com>
2023-06-21 16:48:43 +0200

    calling install script in wordpress dockerfile entrypoint, working

--- srcs/requirements/wordpress/conf/install.sh
+++ srcs/requirements/wordpress/conf/install.sh
@@ -2,3 +2,5 @@
 wp core install --url=kaheinz.42.fr --title=Inception --admin_user=kaheinz --admin_password=docker --admin_email=ka@42.fr --skip-email
 
 wp user create pedro pedro@42.fr --user_pass=docker
+
+/usr/sbin/php-fpm81 -F
